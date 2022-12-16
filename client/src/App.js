0e57e8e75068f3286c2cbcb5e1cfb46b6f34143a
Léa Ifergan <leaifergan15@gmail.com>
2022-12-16 12:15:44 +0100

    auth with firebase

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import CalculPage from "./components/CalculPage";
+import Login from "./components/Login";
+import Reset from "./components/Reset";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
 import { socket, SocketContext } from "./context/socket";
 
 // Print erros if there is some
@@ -18,6 +22,10 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/chat" element={<ChatPage />}></Route>
           <Route path="/calcul" element={<CalculPage />}></Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </SocketContext.Provider>
