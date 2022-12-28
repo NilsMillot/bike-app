073250e351db88040e6e7e838393335a9a089982
NilsMillot <compte.mxs@hotmail.fr>
2022-12-28 12:55:07 +0100

    fix: test test

--- client/src/components/AdminPage.jsx
+++ client/src/components/AdminPage.jsx
@@ -1,7 +1,7 @@
 import React, {useCallback, useEffect, useState} from 'react'
 import { useAuthState } from "react-firebase-hooks/auth";
 import {useNavigate} from "react-router-dom"
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { query, collection, getDocs, where, doc, deleteDoc, setDoc, addDoc } from "firebase/firestore";
 import { auth, db } from "../firebase";;
 
 const AdminPage = () => {
@@ -10,6 +10,16 @@ const AdminPage = () => {
     const [currentUserRoles, setCurrentUserRoles] = useState([]);
     const [isCurrentUserDatasFetched, setIsCurrentUserDatasFetched] = useState(false);
     const [allUsers, setAllUsers] = useState([]);
+    const [allRooms, setAllRooms] = useState([]);
+    // const [shouldModify, setShouldModify] = useState(false);
+    const [newRoom, setNewRoom] = useState({
+      name: '',
+      maxUsers: 0,
+    })
+    const [roomModified, setRoomModified] = useState({
+      name: '',
+      maxUsers: 0,
+    })
 
     const fetchCurrentUserDatas = useCallback(async () => {
       try {
@@ -38,9 +48,19 @@ const AdminPage = () => {
       }
     }, []);
 
-    useEffect(() => {
-      console.log(allUsers);
-    }, [allUsers])
+    // fetch all rooms datas
+    const fetchAllRooms = useCallback(async () => {
+      try {
+        const q = query(collection(db, "rooms"));
+        const doc = await getDocs(q);
+        doc.forEach((doc) => {
+          setAllRooms(prev => [...prev, doc.data()])
+        });
+      } catch (err) {
+        console.error(err);
+        alert("An error occured while fetching user data");
+      }
+    }, []);
 
     // If user is not authenticated by firebase, redirect to login page
     useEffect(() => {
@@ -62,12 +82,94 @@ const AdminPage = () => {
       }
       if (isCurrentUserDatasFetched && currentUserRoles?.includes('admin')) {
         fetchAllUsersDatas()
+        fetchAllRooms()
       }
-    }, [currentUserRoles, navigate, isCurrentUserDatasFetched, fetchAllUsersDatas])
+    }, [currentUserRoles, navigate, isCurrentUserDatasFetched, fetchAllUsersDatas, fetchAllRooms])
+
+    const deleteRoomDoc = useCallback(async(roomName) => {
+      try { 
+        const q = query(collection(db, "rooms"), where("name", "==", roomName));
+        const docFetched = await getDocs(q);
+        const idRoom = docFetched.docs[0].id;
+        await deleteDoc(doc(db, "rooms", idRoom));
+        setAllRooms(allRooms.filter((room) => room.name !== roomName));
+      } catch (err) {
+        console.error(err);
+        alert("An error occured while deleting room");
+      }
+    }, [allRooms])
+
+    const addRoom = useCallback(async() => {
+      try {
+        if (newRoom.name === '') {
+          alert("Room name is required");
+          return;
+        }
+        if (newRoom.maxUsers === 0) {
+          alert("Max users need to be greater than 0");
+          return;
+        }
+        const q = query(collection(db, "rooms"), where("name", "==", newRoom.name));
+        const docFetched = await getDocs(q);
+        if (docFetched.docs.length > 0) {
+          alert("Room already exists");
+        } else {
+          // OK but id with room name isn't good idea
+          // await setDoc(doc(db, "rooms", newRoom.name), {
+          //   name: newRoom.name,
+          //   maxUsers: newRoom.maxUsers,
+          // });
+          await addDoc(collection(db, "rooms"), {
+            name: newRoom.name,
+            maxUsers: newRoom.maxUsers,
+          });
+          setNewRoom({
+            name: '',
+            maxUsers: 0,
+          })
+          allRooms.push(newRoom);
+        }
+      } catch (err) {
+        console.error(err);
+        alert("An error occured while adding room");
+      }
+    }, [newRoom, allRooms])
+
+    const modifyRoom = useCallback(async(previousRoomId) => {
+      try {
+        if (roomModified.name === '') {
+          alert("Room name is required");
+          return;
+        }
+        if (roomModified.maxUsers === 0) {
+          alert("Max users need to be greater than 0");
+          return;
+        }
+        const q = query(collection(db, "rooms"), where("name", "==", roomModified.name));
+        const docFetched = await getDocs(q);
+        if (docFetched.docs.length > 0) {
+          alert("Room already exists");
+        } else {
+          await setDoc(doc(db, "rooms", previousRoomId), {
+            name: roomModified.name,
+            maxUsers: roomModified.maxUsers,
+          });
+          setRoomModified({
+            name: '',
+            maxUsers: 0,
+          })
+          // allRooms.push(roomModified);
+        }
+      } catch (err) {
+        console.error(err);
+        alert("An error occured while adding room");
+      }
+    }, [roomModified])
 
   return (
     <div className="home__container">
         <h1>Panel admin</h1>
+        <h2>All users of our app</h2>
         <table className="admin_table_all_users">
           <thead>
             <tr>
@@ -82,6 +184,7 @@ const AdminPage = () => {
                 <tr key={user.uid}>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
+                  {/* TODO: display roles as checkboxes */}
                   <td>{user?.roles?.map((role) => {
                     return (role + " ")
                   })}</td>
@@ -90,6 +193,91 @@ const AdminPage = () => {
             })}
           </tbody>
         </table>
+        {/* ROOMS PART */}
+        <h2 style={{ marginTop: '20px' }}>All chat rooms</h2>
+        <table className="admin_table_all_users">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>MaxUsers</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allRooms.map((room) => {
+            return (
+              <tr key={room.name}>
+                <td>
+                  {roomModified === room ? (
+                    <input type="text" value={roomModified.name} onChange={
+                      (e) => setRoomModified(prev => ({...prev, name: e.target.value}))
+                    } />
+                  ) : (
+                    room.name
+                  )}
+                </td>
+                <td>
+                  {roomModified === room ? (
+                    <>
+                      <input type="number" value={roomModified.maxUsers} onChange={
+                        (e) => setRoomModified(prev => ({...prev, maxUsers: e.target.value}))
+                      } /> 
+                      <button
+                        onClick={() => {
+                          modifyRoom(room.name);
+                        }}
+                        style={{position: 'absolute', right: '5px'}}
+                      >
+                        ok
+                      </button>
+                    </>
+                  ) :
+                  (
+                    <>
+                      {room.maxUsers}
+                      <button
+                        onClick={() => {
+                          setRoomModified(room);
+                        }}
+                        style={{position: 'absolute', right: '5px'}}
+                      >
+                        modify
+                      </button>
+                    </>
+                  )}
+                  <button
+                    onClick={() => {
+                      deleteRoomDoc(room.name);
+                    }}
+                    style={{position: 'absolute', right: '-60px'}}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            )
+          })}
+          <tr>
+            <td>
+              <input type="text" value={newRoom.name} onChange={
+                (e) => setNewRoom(prev => ({...prev, name: e.target.value}))
+              } />
+            </td>
+            <td>
+            <input type="number" value={newRoom.maxUsers} onChange={
+                (e) => setNewRoom(prev => ({...prev, maxUsers: e.target.value}))
+              } />
+              <button
+                onClick={() => {
+                  addRoom()
+                }}
+                style={{position: 'absolute', right: '5px'}}
+              >
+                add
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
